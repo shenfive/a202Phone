4bafdca91be989635ad49144821ee9ce06876b1b
Danny Shen <shenfive@gmail.com>
2022-04-10 16:01:09 +0800

    FIN

--- a202Phone/ViewController.swift
+++ a202Phone/ViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 class ViewController: UIViewController {
     @IBOutlet weak var phoneTF: UITextField!
     
+    @IBOutlet weak var getCodeBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton!
     @IBOutlet weak var codeTF: UITextField!
     var verID = "AJOnW4SQznPySTx6FeN82vve7d47q77nClOo7OaM_IZNmvuWX5-1ngjA6zjSoCvbzQy729pay8JPGcQxWFJlVMMI1KvkATPBGSQM3DTp2FcjrjtSISpp3PHkRvQL6qtgczv-USxmFECvzod0BCwxHmaknY4fIfFhIy8euwXfjIuAZOHgJd9WLuqn1J0dgrnY0XOu6K6S7vu5ssLaHM9DUu8z8VahamxuKCtlzy-32IOeR8oV6PqHX9k"
     
@@ -21,13 +23,27 @@ class ViewController: UIViewController {
             if let user = user{
                 //已登
                 print("phone:\(user.phoneNumber)")
+                self.phoneTF.isEnabled = false
+                self.codeTF.isEnabled = false
+                self.getCodeBtn.isEnabled = false
+                self.signInBtn.isEnabled = false
+                
             }else{
                 //未登
                 print("未登")
+                self.phoneTF.isEnabled = true
+                self.codeTF.isEnabled = false
+                self.getCodeBtn.isEnabled = true
+                self.signInBtn.isEnabled = false
             }
         }
         
         
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
     @IBAction func getCode(_ sender: Any) {
@@ -41,6 +57,8 @@ class ViewController: UIViewController {
                   print("error:\(error.localizedDescription)")
                 return
               }
+              self.codeTF.isEnabled = true
+              self.signInBtn.isEnabled = true
               print(verificationID)
               self.verID = verificationID ?? ""
               // Sign in using the verificationID and the code sent to the user
@@ -63,5 +81,15 @@ class ViewController: UIViewController {
         
         
     }
+    
+    @IBAction func signOut(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
+    
+    
 }
 
