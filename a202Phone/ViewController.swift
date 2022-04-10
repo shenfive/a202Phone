fb5eff6c1b5e6fa3f63ddc26530b7a012f7734ca
Danny Shen <shenfive@gmail.com>
2022-04-10 15:22:54 +0800

    SignIn

--- a202Phone/ViewController.swift
+++ a202Phone/ViewController.swift
@@ -11,11 +11,23 @@ import Firebase
 class ViewController: UIViewController {
     @IBOutlet weak var phoneTF: UITextField!
     
+    @IBOutlet weak var codeTF: UITextField!
     var verID = "AJOnW4SQznPySTx6FeN82vve7d47q77nClOo7OaM_IZNmvuWX5-1ngjA6zjSoCvbzQy729pay8JPGcQxWFJlVMMI1KvkATPBGSQM3DTp2FcjrjtSISpp3PHkRvQL6qtgczv-USxmFECvzod0BCwxHmaknY4fIfFhIy8euwXfjIuAZOHgJd9WLuqn1J0dgrnY0XOu6K6S7vu5ssLaHM9DUu8z8VahamxuKCtlzy-32IOeR8oV6PqHX9k"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user{
+                //已登
+                print("phone:\(user.phoneNumber)")
+            }else{
+                //未登
+                print("未登")
+            }
+        }
+        
+        
     }
 
     @IBAction func getCode(_ sender: Any) {
@@ -30,12 +42,26 @@ class ViewController: UIViewController {
                 return
               }
               print(verificationID)
-              
+              self.verID = verificationID ?? ""
               // Sign in using the verificationID and the code sent to the user
               // ...
           }
         
     }
     
+    @IBAction func signIn(_ sender: Any) {
+        let code = codeTF.text ?? ""
+        let credential = PhoneAuthProvider.provider().credential(
+          withVerificationID: self.verID,
+          verificationCode: code
+        )
+        Auth.auth().signIn(with: credential)
+        
+        
+        
+        
+        
+        
+    }
 }
 
