bc5a8d6720b64daab22b14c2eb5112fc43921ed1
Danny Shen <shenfive@gmail.com>
2022-04-10 14:41:28 +0800

    Initial Commit

---
+++ a202Phone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a202Phone
+//
+//  Created by 申潤五 on 2022/4/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
