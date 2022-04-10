f55dc6fda3c1a1ea84c950575d1b6eaba89146c0
Danny Shen <shenfive@gmail.com>
2022-04-10 15:03:27 +0800

    Get ID

--- a202Phone/ViewController.swift
+++ a202Phone/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var phoneTF: UITextField!
+    
+    var verID = "AJOnW4SQznPySTx6FeN82vve7d47q77nClOo7OaM_IZNmvuWX5-1ngjA6zjSoCvbzQy729pay8JPGcQxWFJlVMMI1KvkATPBGSQM3DTp2FcjrjtSISpp3PHkRvQL6qtgczv-USxmFECvzod0BCwxHmaknY4fIfFhIy8euwXfjIuAZOHgJd9WLuqn1J0dgrnY0XOu6K6S7vu5ssLaHM9DUu8z8VahamxuKCtlzy-32IOeR8oV6PqHX9k"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getCode(_ sender: Any) {
+        let phoneNum = phoneTF.text ?? ""
+        //檢查電話格式
+        
+        PhoneAuthProvider.provider()
+          .verifyPhoneNumber(phoneNum, uiDelegate: nil) { verificationID, error in
+              if let error = error {
+//                self.showMessagePrompt(error.localizedDescription)
+                  print("error:\(error.localizedDescription)")
+                return
+              }
+              print(verificationID)
+              
+              // Sign in using the verificationID and the code sent to the user
+              // ...
+          }
+        
+    }
+    
 }
 
